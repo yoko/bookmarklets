2e69a50a062277f6d02ccbf130bca9d604d021c6
yoko <yoko@carbonfairy.org>
2012-10-19 00:23:26 +0900

    。。。

--- list-large-images-pixiv.bookmarklet.js
+++ list-large-images-pixiv.bookmarklet.js
@@ -11,7 +11,7 @@ $('img').each(function() {
 	var src = this.dataset.src || this.src;
 	// - /img\d+/: サムネイル
 	// - \d+_s: 削除、マイピク限定を除く
-	if (!(src && (/\/img\d+\/[\w-]+\/\d+_s\.(?:jpg|png|gif)(?:\?\d+)?$/.test(src)))) return;
+	if (!(src && (/\/img\/[\w-]+\/\d+_s\.(?:jpg|png|gif)(?:\?\d+)?$/.test(src)))) return;
 
 	images.push({
 		target: this,
