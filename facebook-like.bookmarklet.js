0f1827f5901dd36018ccdf2cb09d23ab36dc324e
yoko <yoko@carbonfairy.org>
2013-02-20 16:14:39 +0900

    Facebook Like

---
+++ facebook-like.bookmarklet.js
@@ -0,0 +1,15 @@
+// javascript:new function(s,d){d=document;s=d.createElement('script');s.charset='utf-8';s.src='https://raw.github.com/yoko/bookmarklets/master/facebook-like.bookmarklet.js';d.body.appendChild(s)}
+// @title     Facebook Like
+// @include   *
+// @license   MIT License
+
+(function(w, d) {
+
+var url = location.href;
+w.open(
+	'http://yoko.github.com/bookmarklets/facebook-like.html#' + encodeURIComponent(url),
+	null,
+	'width=500,height=300'
+);
+
+})(this, document);
